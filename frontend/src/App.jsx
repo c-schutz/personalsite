df55bc33fee08fbdd36ad19f6ececae1e92cfd4e
c-schutz <cschutz@ufl.edu>
2025-01-27 09:15:25 -0800

    Setup react + nav bar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './Styles.css';
+import './Anim.css';
+import NavBar from './NavBar';
+
+function App() {
+  return (
+    <>
+    <NavBar/>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/NavBar.jsx
@@ -0,0 +1,23 @@
+import './NavStyles.css';
+
+function NavBar() {
+
+    return (
+        <>
+            <nav class='navigation'>
+                <a class='navlinks' href="#">About</a>
+                <a class='navlinks' href="#">Projects</a>
+                <a class='navlinks' href="#">Contact</a>
+            </nav>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc sit amet facilisis fringilla, libero nulla interdum nulla, eu interdum metus nisi in justo. Fusce cursus nulla vel velit vestibulum, ut blandit libero aliquet. Cras faucibus ipsum ut tincidunt dictum. Suspendisse potenti.</p>
+            <p>Quisque at dictum velit. Donec nec mauris vitae libero pretium tristique. Vivamus ac velit nec elit interdum fermentum. Sed scelerisque convallis urna, eget molestie justo convallis sit amet. Nulla facilisi. Integer vel metus vitae felis posuere dictum ut in ligula. Aenean tincidunt sapien nec neque scelerisque, non vehicula lacus suscipit.</p>
+            <p>Nam sit amet ultricies erat. Pellentesque vestibulum, quam vitae euismod sagittis, sapien libero viverra lectus, in tristique nisi urna id enim. Mauris rutrum, mauris vel congue malesuada, nulla ipsum eleifend sapien, in suscipit lacus magna id purus. Integer ut varius dolor. Suspendisse consectetur semper risus a tempor.</p>
+            <p>Fusce in velit sapien. Nullam auctor risus id elit eleifend, id sollicitudin eros bibendum. Ut eleifend porttitor felis, sed tristique augue sodales a. Vivamus euismod, velit at dignissim faucibus, justo lorem cursus lectus, id pharetra nunc purus et odio. Nullam vehicula magna et venenatis rutrum.</p>
+            <p>Etiam luctus leo at viverra porttitor. Integer rhoncus condimentum nisl, ut molestie nulla pharetra ut. Praesent aliquet risus ut enim ultricies, sit amet varius lectus laoreet. Proin bibendum molestie eros, ac iaculis metus vehicula in. Vivamus ultricies quam id risus fermentum laoreet.</p>
+            <p>Curabitur id arcu non nunc feugiat auctor at eget sapien. Donec non sollicitudin urna. Aliquam at sollicitudin tortor. Mauris congue neque vitae purus vehicula, id volutpat lectus porttitor. Cras eget orci a quam scelerisque interdum sed vel sem. Pellentesque id justo ligula. Duis tincidunt tortor ut nulla tristique congue. Nulla sed pharetra justo.</p>
+            <p>Proin tincidunt posuere libero, et scelerisque nunc. Vivamus nec velit eros. Mauris faucibus nulla at justo dictum fringilla. Donec varius efficitur nisl, id aliquam lorem pellentesque quis. Nulla consequat ultricies nisi sit amet sagittis. Donec nec quam nisi. Proin at ligula metus. Suspendisse potenti.</p>
+        </>
+    )
+}
+
+export default NavBar
