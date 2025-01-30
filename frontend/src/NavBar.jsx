b6a43ae0e46545a100f274db34f68b4c73c5c238
c-schutz <cschutz@ufl.edu>
2025-01-30 10:28:23 -0800

    Added scrolling functionality to types

--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -1,14 +1,12 @@
 import './NavStyles.css';
 import { useState, useEffect, useRef, useContext } from 'react';
-import { Context } from './App'; // Import the context
 
 function NavBar() {
-    const [isHidden, setIsHidden] = useState(false);
+    const [isHidden, setIsHidden] = useState(true);
     const lastScrollY = useRef(0); //initially sets last scroll to 0
 
     const handleScroll = () => {//runs on each scroll
         const currentScrollY = window.scrollY; //set current scroll
-        console.log(window.scrollY);
 
         if (currentScrollY > lastScrollY.current || window.scrollY < 150) {//check scroll direction, if near top of page add scrollbar back
             setIsHidden(true);
@@ -35,11 +33,6 @@ function NavBar() {
                 <a className='navlinks' href="#">Projects</a>
                 <a className='navlinks' href="#">Contact</a>
             </nav>
-            <p>Nam sit amet ultricies erat. Pellentesque vestibulum, quam vitae euismod sagittis, sapien libero viverra lectus, in tristique nisi urna id enim...</p>
-            <p>Fusce in velit sapien. Nullam auctor risus id elit eleifend...</p>
-            <p>Etiam luctus leo at viverra porttitor...</p>
-            <p>Curabitur id arcu non nunc feugiat auctor at eget sapien...</p>
-            <p>Proin tincidunt posuere libero, et scelerisque nunc...</p>
         </>
     );
 }
